164a0cdccfd08a1a45a6419fd7a826472baf36d2
lyas <i-miftakhov03@mail.ru>
2021-09-29 12:21:30 +0500

    release v1

---
+++ work1/Schetchik_chisel/Schetchik_chisel.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a[10] = { 0 };
+	char c[10];
+	int i = 0;
+	cout << "Enter your string- " << endl;
+	cin >> c;
+	while (c[i] != '.' && c[i] != '\0')
+	{
+		if ((c[i] >= '0') && (c[i] <= '9'))
+			++a[c[i] - '0'];
+		i++;
+	}
+
+	for (int i = 0; i < 10; i++)
+	
+		cout << "Chislo " << i << " vstrechaetsya " << a[i] << " raz" << endl;
+	
+	return 0;
+}

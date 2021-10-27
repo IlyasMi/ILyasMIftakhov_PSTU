06ac23f0ef8eeff4635c35d26eb4e521a53aea48
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2021-10-27 23:21:01 +0500

    Add files via upload

---
+++ task3.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");//локализируем вывод данных на русский язык
+	int const c = 10;//инициализируем константу
+	cout << c << endl;//выводим на экран
+	int i = 20;//инициализируем переменную
+	cout << i << endl;//выводим ее
+	i = i--;//операция дикремента
+	cout << i << endl;//вывод результата
+
+	return 0;
+}

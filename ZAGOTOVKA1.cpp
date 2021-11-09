707a9da1f164fd49c363f92144d6c5be0e2dbc89
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2021-11-09 14:46:39 +0500

    Add files via upload

---
+++ ZAGOTOVKA1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	cout << "Введите три числа" << endl;
+	cin >> a >> b >> c;
+	if ((a > b) && (a > c))
+	{
+		cout << a << " max" << endl;
+	}
+	else if ((b > c) && (b > a))
+	{
+		cout << b << " max" << endl;
+	}
+	else if((c > a) && (c > b))
+	{
+		cout << c << "c max";
+	}
+	
+	return 0;
+}

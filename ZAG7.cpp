36e5840f96ee1f65a7e9f13c3613c3ebad04ccfb
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2021-11-15 19:20:40 +0500

    Add files via upload

---
+++ ZAG7.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cin >> n;
+	int spaces = n / 2;
+	int stars = 1;
+	if ((n > 3) && (n % 2 != 0))
+	{
+
+		for (int i = 1; i <= (n + 1) / 2; i++)
+		{
+			for (int j = 1; j <= spaces; j++)
+			{
+				cout << " ";
+			}
+			spaces--;
+			for (int j = 1; j <= stars; j++)
+			{
+				cout << "*";
+			}
+			stars += 2;
+			cout << endl;
+		}
+		
+	}
+
+	return 0;
+}

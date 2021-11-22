172592cb7d65e23bbaa0961668adc09352b66ba2
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2021-11-22 19:55:53 +0500

    Add files via upload

---
+++ ZAG15.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, i, tmp;
+	bool answerFound = false;
+
+	cout << "Введите длину последовательности:\n";
+	cin >> n;
+	i = 1;
+
+	while (i <= n && !answerFound)
+	{
+		cin >> tmp;
+		if (tmp == 0)
+		{
+			i++;
+		}
+		else
+		{
+			answerFound = true;
+		}
+	}
+
+	if (tmp > 0)
+	{
+		cout << "Сначала ввели положительное число";
+	}
+	else
+	{
+		cout << "Сначала ввели отрицательное число";
+	}
+
+	return 0;
+}

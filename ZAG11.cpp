59aa3de4fd072f8c6156095bfdae70417d7b7906
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2021-11-22 19:52:45 +0500

    Add files via upload

--- ZAG11.cpp
+++ ZAG11.cpp
@@ -5,23 +5,15 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	int n, i, j;
 	cin >> n;
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		cout << "* ";
-	}
-	cout << endl;
-	for (i = 1; i <= n - 2; i++)
-	{
-		cout << "* ";
-		for (j = 1; j <= n - 2; j++)
+		for (j = 0; j < n; j++)
 		{
-			cout << "  ";
+			cout << "* ";
 		}
-		cout << "*" << endl;
+		cout << endl;
 	}
-	for (i = 1; i <= n; i++)
-	{
-		cout << "* ";
-	}
+
+
 	return 0;
 }

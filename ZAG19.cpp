e7cf0ab6c7d25a4d4f81976d1912f3c5b946e5aa
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2021-11-23 17:44:30 +0500

    Add files via upload

---
+++ ZAG19.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int n, a, b,s;
+	s = 0;
+	cin >> n;
+	while (n != 0)
+	{
+		a = n % 10;
+		n = n / 10;
+		s = s + a;
+	}
+	cout << s;
+	return 0;
+}
---
+++ ZAG22.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, i, s, k, z;
+	s = 0;
+	cin >> n;
+	cin >> z;
+	for (i = 1; i <= n; i++)
+	{
+		if (i % 2 != 0 && i%z == 0)
+		{
+			k = -i;
+			s = s + k;
+		}
+		if (i % 2 == 0 && i%z == 0)
+		{
+			k = i;
+			s = s + k;
+		}
+	}
+	cout << s;
+
+	return 0;
+}

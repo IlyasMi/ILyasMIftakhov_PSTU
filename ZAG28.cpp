38940769c5742441ea943cfe39cd52f24c6bebc1
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2021-11-29 17:48:12 +0500

    Add files via upload

---
+++ ZAG28.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int i, x;
+	bool flag = false;
+	cin >> x;
+	i = x;
+	while (x != 0)
+	{
+		cin >> x;
+		if (i > x&&x != 0)
+		{
+			flag = false;
+			break;
+		}
+		else if (i < x&&x != 0)
+		{
+			flag = true;
+			i = x;
+		}
+	}
+		if (flag == true)
+		{
+			cout << "Correct";
+		}
+		else 
+		{
+			cout << "error";
+		}
+	
+	return 0;
+}

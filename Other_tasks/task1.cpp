949a6ab6d7d6c05539856d2f46e9d9591eeaf5f6
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2021-12-06 19:22:14 +0500

    Add files via upload

---
+++ Other_tasks/task1.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");//локализируем вывод данных на русский язык
+	int i = 1;//инициализируем переменную
+	i = ++i*i++;//мат преоразование
+	cout << i << endl;//вывод данных на экран
+
+	return 0;
+}
---
+++ Other_tasks/task7.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");//локализуем вывод данных на русский язык
+	int arr[5];//массив с размером 5
+	for (int i = 0; i < 5; i++)//цикл счетчик
+	{
+		cin >> arr[i];	//заполнение массива
+	}
+	for (int i = 4; i >= 0; i--)// цикл счетчик
+	{
+		cout << arr[i] << " " << endl;//вывод массива в обратном порядке
+	}
+	return 0;
+}

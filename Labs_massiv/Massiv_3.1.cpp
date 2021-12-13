0ad6b5cd1344f8edba5cdb39cfd26626aec6260e
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2021-12-13 22:10:44 +0500

    Add files via upload

---
+++ Labs_massiv/Massiv_3.1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+// #include <stdlib.h>
+#include <cmath>
+	// int arr[n];
+	// int r, range_min, range_max;
+	// range_min = 2;
+	// range_max = 412;
+	// for (int i = 0; i < n; i++)
+	// {
+	//     arr[i] = ((double)rand() / RAND_MAX) * (range_max - range_min) + range_min;
+	//     cout << arr[i] << " ";
+	// }
+	// cout << endl;
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n = 6; int key;
+	int arr[6] = { 5, 2, 4, 6, 1, 3 };
+	for (int i = 0; i < n; i++)
+	{cout << arr[i] << " ";}
+	cout << endl;
+	for (int j = 1; j < n; j++)
+	{
+		key = arr[j];
+		cout << "Key: " << key << "\n";
+		int i = j - 1;
+		cout << "Вошли в цикл\n";
+		while (i >= 0 && arr[i] > key)
+		{
+			arr[i + 1] = arr[i];
+			cout << "Надо обмен!\n";
+			cout << "Массив сейчас: ";
+			for (int i = 0; i < n; i++)
+			{
+				cout << arr[i] << " ";
+			}
+			i--;
+		}
+		cout << "Вышли из цикла\n";
+		arr[i + 1] = key;
+		cout << "Массив сейчас: ";
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
---
+++ Labs_massiv/Massiv_3.8-v2.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<stdlib.h>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int n, tmp;
+	n = 11;
+	int a[11];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+
+	}
+	cout << endl;
+	for (int i = 0; i < n/2; i++)//SWAP
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
+	
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << endl;
+	}
+
+	return 0;
+}

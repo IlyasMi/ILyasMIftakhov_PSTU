58af2d0add020f500c5156121f2ae0780c66e475
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2021-12-21 13:53:00 +0500

    Add files via upload

---
+++ Sortirovka/3.1.cpp
@@ -0,0 +1,29 @@
+﻿// Два стакана
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 10;
+	int stakan=0;
+	int a[10] = {9,5,2,18,28,64,4,0,10,12};
+	
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	for (int j = 1; j < n; j++)
+	{
+		stakan = a[j];
+		int i = j - 1;
+		while (i >= 0 && a[i] > stakan)
+		{
+			a[i + 1] = a[i];
+			i--;
+		}
+		a[i + 1] = stakan;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+}
---
+++ Sortirovka/3.2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n = 10;
+	int miniDX = 0;
+	int stakan = 0;
+	int a[10] = { 7,6,9,30,21,3,0,15,18,100 };
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		miniDX = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (a[j] < a[miniDX])
+			{
+				miniDX = j;
+			}
+		}
+		stakan = a[miniDX];
+		a[miniDX] = a[i];
+		a[i] = stakan;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+	cout << endl;
+
+}
